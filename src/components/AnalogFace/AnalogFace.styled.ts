424e5835ecb6cc1c3dc2546a5ad3d83aed361711
Stephen Wood <absolutholz@gmail.com>
2021-08-15 19:32:54 +0200

    Update analog face hands

--- src/components/AnalogFace/AnalogFace.styled.ts
+++ src/components/AnalogFace/AnalogFace.styled.ts
@@ -6,20 +6,7 @@ export const Face = styled.div`
     position: absolute;
     top: 0;
     width: 100%;
-
-    &::after {
-        aspect-ratio: 1 / 1;
-        background: black;
-        border-radius: 50%;
-        content: "";
-        display: block;
-        left: 50%;
-        position: absolute;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        width: max(5%, 0.75rem);
-    }
-`;
+    `;
 
 const Hand = styled.span`
   border-radius: 9rem;
@@ -27,11 +14,24 @@ const Hand = styled.span`
   position: absolute;
   bottom: 50%;
   transform-origin: 50% 100%;
+
+  &::after {
+        aspect-ratio: 1 / 1;
+        background: currentColor;
+        border-radius: 50%;
+        content: "";
+        display: block;
+        left: 50%;
+        position: absolute;
+        top: 100%;
+        transform: translate(-50%, -50%);
+        width: 150%;
+    }
 `;
 
 interface IHourHandProps {
-    hours: number;
-    minutes: number;
+  hours: number;
+  minutes: number;
 }
 
 export const HourHand = styled(Hand)<IHourHandProps>`
@@ -41,7 +41,8 @@ export const HourHand = styled(Hand)<IHourHandProps>`
     css`
       transform: rotate(${(hours / 12) * 360 + (minutes / 60) * 30}deg);
     `}
-    background: var(--highlight, hsl(0deg 68% 42%));
+    background: currentColor;
+    color: var(--highlight, hsl(0deg 68% 42%));
     left: 48%;
     padding-bottom: 25%;
     width: max(4%, 0.5rem);
